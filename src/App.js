1743eaf2bdff14d559b1956ee9972883d29164fa
jlum85 <jlum@wanadoo.fr>
2019-11-10 19:47:10 +0100

    Améioration design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import Search from "./images/search.png";
 import "./App.css";
 
 // tableau de données exemples pour l'initialisation, on stocke chaque tache sous forme d'objet avec les attributs suivants
 // name : nom de la tache
 // done : true / false si true le texte sera barré
 const tab = [
-  { name: "Faire les courses", done: true },
-  { name: "Sortir le chien", done: false }
+  { name: "Publier ToDo list sur Netlify", done: true },
+  { name: "Réaliser un projet démineur", done: false }
 ];
 
 function App() {
@@ -16,14 +17,18 @@ function App() {
 
   const handleSubmit = event => {
     event.preventDefault();
-    // copy du tableau d'origine et ajout du nouvel élément
-    const newTasks = [...tasks];
-    newTasks.push({ name: taskInput, done: false });
-    setTasks(newTasks);
+
+    if (taskInput.trim() !== "") {
+      // Copie du tableau d'origine et ajout du nouvel élément
+      const newTasks = [...tasks];
+      newTasks.push({ name: taskInput, done: false });
+      setTasks(newTasks);
+    }
+    setTaskInput("");
   };
 
   const onDeleteClick = indexToDel => {
-    // on récupère un nouveau tableau qui contient tout sauf l'indice qui correspond à indexToDel
+    // On récupère un nouveau tableau qui contient tout sauf l'indice qui correspond à indexToDel
     const newTasks = tasks.filter((item, index) => index !== indexToDel);
     setTasks(newTasks);
   };
@@ -47,39 +52,89 @@ function App() {
     const taskAll = [...taskActive, ...taskDone];
 
     // 3° On passe par un map pour générer les tasks à afficher
-    const element = taskAll.map((item, index) => {
-      return (
-        <div key={index} className="taskItem">
-          <span className="btnDel" onClick={() => onDeleteClick(index)}>
-            X
-          </span>
-          <span
-            className={"myTask " + (item.done ? "taskDone" : "")}
-            onClick={() => onItemClick(index)}
-          >
-            {item.name}
-          </span>
-        </div>
-      );
-    });
-    return element; // liste des taches à afficher après recherche et tri
+    if (taskAll.length > 0) {
+      const element = taskAll.map((item, index) => {
+        return (
+          <div key={index} className="taskItem">
+            <svg
+              className="btnDel"
+              onClick={() => onDeleteClick(index)}
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ccc"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <line x1="15" y1="9" x2="9" y2="15"></line>
+              <line x1="9" y1="9" x2="15" y2="15"></line>
+            </svg>
+            <span
+              className={"myTask " + (item.done ? "taskDone" : "")}
+              onClick={() => onItemClick(index)}
+            >
+              {item.name}
+            </span>
+          </div>
+        );
+      });
+      return <div className="listItems">{element} </div>;
+    }
   };
 
   return (
     <div className="App">
       <h1> To-Do list</h1>
-      <input
-        className="search"
-        onChange={event => setTaskFilter(event.target.value)}
-        type="text"
-      />
 
-      <div className="listItems">{getTasks()}</div>
+      <div className="container">
+        <div style={{ position: "relative" }}>
+          <input
+            className="search"
+            onChange={event => setTaskFilter(event.target.value)}
+            type="text"
+            value={taskFilter}
+          />
+          <img
+            className="searchImg"
+            style={{
+              left: 5,
+              top: "16px"
+            }}
+            src={Search}
+            alt="search"
+          />
+          <svg
+            className="clearSearch"
+            onClick={() => setTaskFilter("")}
+            style={{ position: "absolute", right: 5, top: "21px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#ccc"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="10"></circle>
+            <line x1="15" y1="9" x2="9" y2="15"></line>
+            <line x1="9" y1="9" x2="15" y2="15"></line>
+          </svg>
+        </div>
+      </div>
+
+      {/* <div className="listItems">{getTasks()} </div> */}
+      {getTasks()}
       <div className="todoForm">
         <form onSubmit={handleSubmit}>
           <input
             className="inputTask"
-            placeholder="Titre"
+            placeholder="Nouvelle tâche"
             type="text"
             value={taskInput}
             onChange={event => setTaskInput(event.target.value)}
